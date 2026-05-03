2b95189249a4d1af28629f4451e56e3b0fb5faf5
aritra <aritra20192020@gmail.com>
2026-05-04 01:38:21 +0530

    rrefactor

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from config import get_settings
+import uvicorn
+
+if __name__ == "__main__":
+    settings = get_settings()
+    uvicorn.run(
+        "main:app",
+        host=settings.HOST,
+        port=settings.PORT,
+    )
